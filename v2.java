a0fd664456bd01dbdc88b56529ff27e7ae67c37a
Igor <130934354+igorcodigo@users.noreply.github.com>
2023-11-11 14:40:09 -0300

    Update v2.java

--- v2.java
+++ v2.java
@@ -1,3 +1,12 @@
+class Livro {
+   private String titulo;
+   private String autor;
+
+   public Livro(String titulo, String autor) {
+       this.titulo = titulo;
+       this.autor = autor;
+   }
+
 class Usuario {
    private String nome;
 
